cc393d51e3df5a16fd0721f7120b8e6418089702
Paransh Marwari <167611594+Paransh9@users.noreply.github.com>
2025-04-29 03:41:42 +0530

    Update Tarakk_demo.sql

--- Containerized MySQL, Agile & Efficient/Tarakk_demo.sql
+++ Containerized MySQL, Agile & Efficient/Tarakk_demo.sql
@@ -1,11 +1,1 @@
-CREATE DATABASE Paransh;
-use Paransh;
-CREATE TABLE students(
- 
-StudentID int not null AUTO_INCREMENT, 
-FirstName varchar(100) NOT NULL,
-Surname varchar(100) NOT NULL,
-PRIMARY KEY (StudentID)
-);
-INSERT INTO students (FirstName, Surname)
-VALUES("Paransh", "Marwari"), ("Aryan", "Soni");
+
